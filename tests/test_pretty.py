2cffafb2cc91d1294aae9cceb61228d481152f68
刘项 <lexdene@users.noreply.github.com>
2022-08-06 18:18:40 +0800

    support function (#3)

--- tests/test_pretty.py
+++ tests/test_pretty.py
@@ -2,6 +2,7 @@ from textwrap import dedent
 from unittest import TestCase
 
 from pangolier.metrics import Metric
+from pangolier.functions import Rate, Sum
 
 
 class TestPretty(TestCase):
@@ -28,5 +29,103 @@ class TestPretty(TestCase):
                     job="prometheus",
                     group="canary"
                 }
+            '''
+        )
+
+    def test_rate(self):
+        self._assert_pretty_equal(
+            Rate(Metric('http_requests_total'), timespan='5m'),
+            '''
+                rate(
+                    http_requests_total[5m]
+                )
+            '''
+        )
+
+    def test_rate_with_filter(self):
+        self._assert_pretty_equal(
+            Rate(
+                Metric('http_requests_total').filter(
+                    job='prometheus',
+                    group='canary'
+                ),
+                timespan='5m'
+            ),
+            '''
+                rate(
+                    http_requests_total{
+                        job="prometheus",
+                        group="canary"
+                    }[5m]
+                )
+            '''
+        )
+
+    def test_sum(self):
+        self._assert_pretty_equal(
+            Sum(Metric('http_requests_total')),
+            '''
+                sum(
+                    http_requests_total
+                )
+            '''
+        )
+
+    def test_sum_by(self):
+        self._assert_pretty_equal(
+            Sum(
+                Metric('http_requests_total'),
+                by=('job', 'group'),
+            ),
+            '''
+                sum by(
+                    job, group
+                )(
+                    http_requests_total
+                )
+            '''
+        )
+
+    def test_sum_by_rate(self):
+        self._assert_pretty_equal(
+            Sum(
+                Rate(
+                    Metric('http_requests_total'),
+                    timespan='5m'
+                ),
+                by=('job', 'group'),
+            ),
+            '''
+                sum by(
+                    job, group
+                )(
+                    rate(
+                        http_requests_total[5m]
+                    )
+                )
+            '''
+        )
+
+    def test_sum_by_rate_with_filter(self):
+        self._assert_pretty_equal(
+            Sum(
+                Rate(
+                    Metric('http_requests_total').filter(
+                        job='prometheus',
+                    ),
+                    timespan='5m'
+                ),
+                by=('group',)
+            ),
+            '''
+                sum by(
+                    group
+                )(
+                    rate(
+                        http_requests_total{
+                            job="prometheus"
+                        }[5m]
+                    )
+                )
             '''
         )
