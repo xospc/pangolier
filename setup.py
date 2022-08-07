6ffea1d8c59a3064460129a9d04049d47033fce1
Elephant Liu <lexdene@gmail.com>
2022-08-07 10:45:20 +0800

    upload to pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='pangolier',
     version='0.0.1',
-    packages=find_packages(),
+    packages=find_packages(include=['pangolier']),
     python_requires='>3.10.0',
+
+    url='https://github.com/lexdene/pangolier',
+    description="prometheus query builder",
+    long_description_content_type='text/markdown',
+    long_description=long_description,
 )
