0ca3baf7208dd0bbbdcdd19fe578c8b1778dd89a
Elephant Liu <lexdene@users.noreply.github.com>
2023-10-09 11:40:24 +0800

    add FilterableMetricBase (#20)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='pangolier',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(include=['pangolier']),
     package_data={
         'pangolier': ['py.typed'],
