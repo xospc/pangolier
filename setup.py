24d839df84ca7e509a21695b5fc9e3759bd3075c
Elephant Liu <lexdene@gmail.com>
2024-03-24 17:28:22 +0800

    update setup

--- setup.py
+++ setup.py
@@ -10,10 +10,15 @@ setup(
     package_data={
         'pangolier': ['py.typed'],
     },
-    python_requires='>3.10.0',
+    python_requires='>=3.10',
 
-    url='https://github.com/lexdene/pangolier',
-    description='prometheus query builder',
+    url='https://pangolier.readthedocs.io',
+    description='build PromQL by Python code',
     long_description_content_type='text/markdown',
     long_description=long_description,
+
+    project_urls={
+        'Documentation': 'https://pangolier.readthedocs.io',
+        'Source': 'https://github.com/lexdene/pangolier',
+    },
 )
